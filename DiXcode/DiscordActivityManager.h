6bb40096a8b22c67b83ab0fa4dacac90ecb2716d
Raul Max <raulmax319@gmail.com>
2023-02-27 00:48:46 -0300

    feat: objc++ discord activity manager impl to make use of the sdk

--- DiXcode/DiscordActivityManager.h
+++ DiXcode/DiscordActivityManager.h
@@ -5,13 +5,55 @@
 // Copyright © 2023 Raul Max. All rights reserved.
 //
   
+#ifndef DISCORD_ACTIVITY_MANAGER_H
+#define DISCORD_ACTIVITY_MANAGER_H
+
+#ifdef __cplusplus
+#include <iostream>
+#include <string>
+
+#include <thread>
+#include <chrono>
+
+#include "discord.h"
+#include "ActivityManager.h"
+#include "Constants.h"
+#endif
 
 #import <Foundation/Foundation.h>
+#import "AppleScriptManager.h"
 
-NS_ASSUME_NONNULL_BEGIN
 
 @interface DiscordActivityManager : NSObject
 
+@property NSInteger clientId;
+@property ActivityManager* manager;
+@property AppleScriptManager* appleScript;
+
+- (id) initWithClientId:(NSInteger)clientId;
+
+/**
+ Run the application
+ */
+- (int) run;
+
+/**
+ What are we doing.
+ */
+- (void) setDetails:(NSString*)detauls;
+
+/**
+ The workspace we are working in.
+ */
+- (void) setWorkspace:(NSString*)workspace;
+
+- (void) setSmallImage:(NSString*)image;
+- (void) setLargeImage:(NSString*)image;
+- (void) setSmallText:(NSString*)text;
+- (void) setLargeText:(NSString*)text;
+- (void) startTimer;
+- (void) setActivityType;
+
 @end
 
-NS_ASSUME_NONNULL_END
+#endif /* DISCORD_ACTIVITY_MANAGER_H */
--- DiXcode/DiscordActivityManager.mm
+++ DiXcode/DiscordActivityManager.mm
@@ -1,5 +1,5 @@
 //
-// DiscordActivityManager.m
+// DiscordActivityManager.mm
 //
 // Created by Raul Max on 26/02/23.
 // Copyright © 2023 Raul Max. All rights reserved.
@@ -9,5 +9,73 @@
 #import "DiscordActivityManager.h"
 
 @implementation DiscordActivityManager
+
+- (id) initWithClientId:(NSInteger)clientId {
+  _manager = new ActivityManager(clientId);
+  _appleScript = [[AppleScriptManager alloc] init];
+
+  return self;
+}
+
+- (int) run {
+  _manager->createState();
+
+  [self startTimer];
+
+  while(true) {
+    NSString* activeWindow = [_appleScript getActiveFileName];
+    NSString* workspace = [_appleScript getActiveWorkspace];
+    NSString* fileExtension = [activeWindow componentsSeparatedByString:@"."].lastObject;
+    NSString* fileName = Constants::getDetailedName(fileExtension);
+
+    NSLog(@"File extension: %@", fileExtension);
+    NSLog(@"File name: %@", fileName);
+
+    [self setDetails:[[NSString alloc] initWithFormat:@"Editing %@", activeWindow]];
+    [self setWorkspace:[[NSString alloc] initWithFormat:@"Workspace: %@", workspace]];
+    [self setLargeText:[[@"Editing " stringByAppendingString:fileName] stringByAppendingString:@" file."]];
+
+    [self setSmallText:@"Xcode"];
+    [self setSmallImage:@"xcode"];
+    [self setLargeImage:fileExtension];
+
+    _manager->updateActivity();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
+  return 0;
+}
+
+- (void) setDetails:(NSString*)details {
+  _manager->setDetails([details cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) setWorkspace:(NSString*)workspace {
+  _manager->setWorkspace([workspace cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) setSmallImage:(NSString*)image {
+  _manager->setSmallImage([image cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) setLargeImage:(NSString*)image {
+  _manager->setLargeImage([image cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) setSmallText:(NSString*)text {
+  _manager->setSmallText([text cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) setLargeText:(NSString*)text {
+  _manager->setLargeText([text cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+- (void) startTimer {
+  _manager->startTimer();
+}
+
+- (void) setActivityType {
+  _manager->setActivityType();
+}
 
 @end
