55c0cb2f58c7f92625abecc137e0b18c589a5c4e
Raul Max <raulmax319@gmail.com>
2023-02-27 00:43:29 -0300

    feat: cpp class implementation for discord activity

--- DiXcode/ActivityManager.cpp
+++ DiXcode/ActivityManager.cpp
@@ -4,6 +4,75 @@
 // Created by Raul Max on 26/02/23.
 // Copyright © 2023 Raul Max. All rights reserved.
 //
-  
 
-#include "ActivityManager.hpp"
+#include "ActivityManager.h"
+
+namespace {
+volatile bool interrupted { false };
+}
+
+ActivityManager::ActivityManager(long int clientId): clientId(clientId) {}
+
+void ActivityManager::createState() {
+  discord::Core::Create(clientId, DiscordCreateFlags_Default, &core);
+
+  state.core.reset(core);
+
+  if (!state.core) {
+    std::cout << "Failed to create a Discord instance.";
+    std::exit(-1);
+  }
+}
+
+void ActivityManager::updateActivity() {
+  state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
+    std::cout << ((result == discord::Result::Ok) ? "Succeeded in " : "Failed") << "updating activity." << std::endl;
+    interrupted = true;
+  });
+
+  runCallbacks();
+}
+
+void ActivityManager::runCallbacks() {
+  signal(SIGINT, [](int) {
+    interrupted = true;
+  });
+
+  if (!interrupted) {
+    core->RunCallbacks();
+    std::this_thread::sleep_for(std::chrono::milliseconds(16));
+  }
+}
+
+void ActivityManager::setDetails(char const* details) {
+  activity.SetDetails(details);
+}
+
+void ActivityManager::setWorkspace(char const* state) {
+  activity.SetState(state);
+}
+
+void ActivityManager::setLargeText(char const* text) {
+  activity.GetAssets().SetLargeText(text);
+}
+
+void ActivityManager::setSmallText(char const* text) {
+  activity.GetAssets().SetSmallText(text);
+}
+
+void ActivityManager::setLargeImage(char const* name) {
+  activity.GetAssets().SetLargeImage(name);
+}
+
+void ActivityManager::setSmallImage(char const* name) {
+  activity.GetAssets().SetSmallImage(name);
+}
+
+void ActivityManager::startTimer() {
+  std::time_t currentTime = std::time(0);
+  activity.GetTimestamps().SetStart(currentTime);
+}
+
+void ActivityManager::setActivityType() {
+  activity.SetType(discord::ActivityType::Playing);
+}
--- DiXcode/ActivityManager.h
+++ DiXcode/ActivityManager.h
@@ -1,14 +1,85 @@
 //
-// ActivityManager.hpp
+// ActivityManager.h
 //
 // Created by Raul Max on 26/02/23.
 // Copyright © 2023 Raul Max. All rights reserved.
 //
   
 
-#ifndef ActivityManager_hpp
-#define ActivityManager_hpp
+#ifndef ACTIVITY_MANAGER_H
+#define ACTIVITY_MANAGER_H
 
-#include <stdio.h>
+#include <iostream>
+#include <thread>
 
-#endif /* ActivityManager_hpp */
+#include "DiscordState.h"
+
+class ActivityManager {
+  long int clientId;
+  discord::Core* core {};
+  DiscordState state {};
+
+  ActivityManager() = default;
+  ~ActivityManager() = default;
+public:
+  ActivityManager(long int clientId);
+  discord::Activity activity {};
+
+  /**
+   Creates the state needed to run the RPC Server.
+   */
+  void createState();
+
+  /**
+   What are we doing.
+   */
+  void setDetails(char const*);
+
+  /**
+   The workspace we are working in.
+   */
+  void setWorkspace(char const*);
+
+  /**
+   The image presented in the current activity.
+   */
+  void setLargeImage(char const*);
+
+  /**
+   The image presented in the bottom right of the current activity.
+   */
+  void setSmallImage(char const*);
+
+  /**
+   The text decoration for the current activity.
+   */
+  void setLargeText(char const*);
+
+  /**
+   The text decoration for the small image of the current activity.
+   */
+  void setSmallText(char const*);
+
+  /**
+   Starts the timer for the current activity.
+   */
+  void startTimer();
+
+  /**
+   Set the activity type (defaults to Playing).
+   I don't intent to change this unless they add a new type.
+   */
+  void setActivityType();
+
+  /**
+   Updates the current activity with a new updated data.
+   */
+  void updateActivity();
+
+  /**
+   Run all callbacks in order to update the new activity.
+   */
+  void runCallbacks();
+};
+
+#endif /* ACTIVITY_MANAGER_H */
