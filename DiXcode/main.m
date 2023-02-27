ab8e0413b21933101dd1e7aa1d0f4a12e46af518
Raul Max <raulmax319@gmail.com>
2023-02-26 23:10:54 -0300

    Initial Commit

---
+++ DiXcode/main.m
@@ -0,0 +1,17 @@
+//
+// main.m
+//
+// Created by Raul Max on 26/02/23.
+// Copyright © 2023 Raul Max. All rights reserved.
+//
+  
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
